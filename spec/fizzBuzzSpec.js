df81f1d9e05a97ba30f1f63a24af83a89ad18935
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-07-22 15:50:21 +0100

    tests are passed, got rid off player and song

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,15 +1,17 @@
 describe('FizzBuzz', function() {
 
-    var FizzBuzz;
-
+    var fizzBuzz;
+  
     beforeEach(function() {
-        fizzBuzz = new FizzBuzz();
+      fizzBuzz = new FizzBuzz();
     });
-    describe('mulpiples of 3', function() {
-       it('fizzes for 3', function() {
+  
+    describe('multiples of 3', function() {
+      it('fizzes for 3', function() {
         expect(fizzBuzz.play(3)).toEqual('Fizz');
-       }); 
-       it('fizzes for 6', function() {
+      });
+  
+      it('fizzes for 6', function() {
         expect(fizzBuzz.play(6)).toEqual('Fizz');
       });
     });
